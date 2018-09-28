0e9d4c3e0221bec3d5ac19549b3a2758b7cd6579
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-09-28 06:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if string === string.toLowerCase
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }
 }
